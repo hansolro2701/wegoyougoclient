1f8be03f8baac9fd854c913a3a671c8765e8b6cc
hansolro2701 <hansolro2701@crecolto.com>
2021-06-25 14:12:11 +0900

    kkkkkkk

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ const target = 'http://127.0.0.1:3000';
 
 module.exports = {
   devServer: {
-    port: 8080,
-    proxy: {
+      proxy: {
       '^/api': {
         target,
         changeOrigin: true
